8d6d2bb190e2cc617b96b8a8f590205a49cf6686
Alfinfdw <113610587+Alfinfdw@users.noreply.github.com>
2023-01-25 12:24:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -33,24 +33,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Haruka" //namabot kalian
-global.ownername = "Nadliww" //nama kalian
+global.botname = "DivanPtra" //namabot kalian
+global.ownername = "DivSan" //nama kalian
 global.myweb = "https://s.id/nadliww" //bebas asal jan hapus
 global.youtube = "https://youtube.com/pewdiepie" //bebas asal jan hapus
 global.myweb2 = "https://s.id/nadliww" //bebas
-global.email = "siorangjero@gmail.com" //bebas
+global.email = "DivanPtra@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62895701081582'] //premium user
-global.owner = ["62895701081582", "62895701081582"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62895701081582" // nomor wa kalian
-global.ownernomerr = "62895701081582" //nmr wa kalian
+global.premium = ['447971533162'] //premium user
+global.owner = ["447971533162", "6285608468994"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "447971533162" // nomor wa kalian
+global.ownernomerr = "6285608468994" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = 'Haruka' //sticker wm ubah
-global.author = 'Nadliww' //sticker wm ganti nama kalian
+global.packname = 'DivanPtra' //sticker wm ubah
+global.author = 'DivanPtra' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
